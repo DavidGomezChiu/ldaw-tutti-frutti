13859372150485a18f98ac42405ddb603c683042
David Gómez Chiu <david.gomez.chiu@gmail.com>
2020-04-07 23:50:46 -0500

    CharacterSelection reconoce a qué animal se le da clic

--- tutti-frutti-front/src/components/CharacterSelector/CharacterSelector.js
+++ tutti-frutti-front/src/components/CharacterSelector/CharacterSelector.js
@@ -3,19 +3,21 @@ import './CharacterSelector.scss';
 import Card from 'react-bootstrap/Card';
 
 export class CharacterSelector extends Component {
+    selectAnimal = (event) => {
+        this.props.onSelect(event.target.name);
+    }
+
     render() {
         return (
             <div>
                 <Card className="card mx-auto">
                     <Card.Body>
                         <Card.Title className="mb-3">Escoge un avatar</Card.Title>
-                        <div>
-                            <img className="animal-icon" alt="Perezoso" src={process.env.PUBLIC_URL+'/assets/icons/sloth.svg'}/>
-                            <img className="animal-icon" alt="Oso" src={process.env.PUBLIC_URL+'/assets/icons/bear.svg'}/>
-                            <img className="animal-icon" alt="Perro" src={process.env.PUBLIC_URL+'/assets/icons/dog.svg'}/>
-                            <img className="animal-icon" alt="Pato" src={process.env.PUBLIC_URL+'/assets/icons/duck.svg'}/>
-                            <img className="animal-icon" alt="Caballo" src={process.env.PUBLIC_URL+'/assets/icons/horse.svg'}/>
-                        </div>
+                        <img name="sloth" onClick={this.selectAnimal.bind(this)} className="animal-icon" alt="Perezoso" src={process.env.PUBLIC_URL+'/assets/icons/sloth.svg'}/>
+                        <img name="bear" onClick={this.selectAnimal.bind(this)} className="animal-icon" alt="Oso" src={process.env.PUBLIC_URL+'/assets/icons/bear.svg'}/>
+                        <img name="dog" onClick={this.selectAnimal.bind(this)} className="animal-icon" alt="Perro" src={process.env.PUBLIC_URL+'/assets/icons/dog.svg'}/>
+                        <img name="duck" onClick={this.selectAnimal.bind(this)} className="animal-icon" alt="Pato" src={process.env.PUBLIC_URL+'/assets/icons/duck.svg'}/>
+                        <img name="horse" onClick={this.selectAnimal.bind(this)} className="animal-icon" alt="Caballo" src={process.env.PUBLIC_URL+'/assets/icons/horse.svg'}/>
                     </Card.Body>
                 </Card>
             </div>
--- tutti-frutti-front/src/views/CharacterSelection/CharacterSelection.js
+++ tutti-frutti-front/src/views/CharacterSelection/CharacterSelection.js
@@ -6,13 +6,17 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export class CharacterSelection extends Component {
+    chooseAnimal(animal){
+        console.log(animal);
+    }
+
     render() {
         return (
             <div className="content">
                 <Container>
                     <Row className="justify-content-center">
                         <Col>
-                            <CharacterSelector className="selector"></CharacterSelector>
+                            <CharacterSelector onSelect={this.chooseAnimal.bind(this)} className="selector"></CharacterSelector>
                         </Col>
                     </Row>
                 </Container>
