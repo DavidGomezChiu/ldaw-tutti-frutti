add7c4c4be13cc07499c6f7b819eb8bea592aabe
David Gómez Chiu <david.gomez.chiu@gmail.com>
2020-04-10 02:35:09 -0500

    Vista de 404

--- tutti-frutti-front/src/views/Error404/Error404.js
+++ tutti-frutti-front/src/views/Error404/Error404.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Error404.scss'
 
 export class Error404 extends Component {
     render() {
         return (
             <div>
-                
+                <h2>No encontramos lo que buscabas :/</h2>
+                <Link to={'/'}>Pero, ¿por qué no juegas basta? <span role="img" aria-label="wink">😉</span></Link>
             </div>
         )
     }
